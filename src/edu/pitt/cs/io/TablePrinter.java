f428262b08a9182534006a30a908805e4d8c57ac
Fan <jimzhangsklse@gmail.com>
2015-10-18 21:48:47 -0400

    add csv

--- src/edu/pitt/cs/io/TablePrinter.java
+++ src/edu/pitt/cs/io/TablePrinter.java
@@ -11,6 +11,8 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -19,39 +21,45 @@ import edu.pitt.cs.model.Table;
 
 /**
  * Printing the table to screen, txt file or excel files
+ * 
  * @author zhangfan
  *
  */
 public class TablePrinter {
 	/**
 	 * Print to screen
+	 * 
 	 * @param table
 	 * @throws IOException
 	 */
 	public static void printScreen(Table table) throws IOException {
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		print(writer,table);
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+				System.out));
+		print(writer, table);
 		writer.flush();
 		writer.close();
 	}
 
 	/**
 	 * Print to txt files
+	 * 
 	 * @param table
 	 * @param fileName
 	 * @throws IOException
 	 */
-	public static void printFile(Table table, String fileName) throws IOException {
+	public static void printFile(Table table, String fileName)
+			throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-		print(writer,table);
+		print(writer, table);
 		writer.close();
 	}
-	
-	public static void print(BufferedWriter writer, Table table) throws IOException {
+
+	public static void print(BufferedWriter writer, Table table)
+			throws IOException {
 		List<String> cols = table.getColumns();
-		
+
 		for (int i = 0; i < cols.size(); i++) {
-			writer.write(cols.get(i)+"\t");
+			writer.write(cols.get(i) + "\t");
 		}
 
 		writer.write("\n");
@@ -59,19 +67,39 @@ public class TablePrinter {
 		for (int i = 0; i < rows.size(); i++) {
 			List<String> row = rows.get(i);
 			for (int j = 0; j < row.size(); j++) {
-				writer.write(row.get(j)+"\t");
+				writer.write(row.get(j) + "\t");
 			}
 			writer.write("\n");
 		}
 	}
 
+	public static void printCSV(Table table, String csvFile) throws IOException {
+		List<String> cols = table.getColumns();
+		FileWriter fileWriter = null;
+		CSVPrinter csvFilePrinter = null;
+		CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator("\n");
+
+		fileWriter = new FileWriter(csvFile);
+		csvFilePrinter = new CSVPrinter(fileWriter, csvFileFormat);
+		csvFilePrinter.printRecord(cols);
+		List<List<String>> rows = table.getRows();
+		for (int i = 0; i < rows.size(); i++) {
+			csvFilePrinter.printRecord(rows.get(i));
+		}
+		fileWriter.flush();
+		fileWriter.close();
+		csvFilePrinter.close();
+	}
+
 	/**
 	 * Print to excel files
+	 * 
 	 * @param table
 	 * @param excelFile
 	 * @throws IOException
 	 */
-	public static void printExcel(Table table, String excelFile) throws IOException {
+	public static void printExcel(Table table, String excelFile)
+			throws IOException {
 		FileOutputStream fileOut = new FileOutputStream(excelFile);
 		XSSFWorkbook xwb = new XSSFWorkbook();
 
